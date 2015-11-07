2ba15c7e13cb1914b0fba8e8f68a216f53dd9956
ramya radhakrishnan <ramyaradhakrishnan@ramyas-macbook-pro.local>
2015-11-07 13:08:45 -0800

    Initial Commit

---
+++ potHolesTests/potHolesTests.swift
@@ -0,0 +1,36 @@
+//
+//  potHolesTests.swift
+//  potHolesTests
+//
+//  Created by ramya radhakrishnan on 11/7/15.
+//  Copyright © 2015 ramya radhakrishnan. All rights reserved.
+//
+
+import XCTest
+@testable import potHoles
+
+class potHolesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
